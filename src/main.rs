da9288e3a3987be572a9b4ac8d65901bbc076949
xhresko <juraj.hresko@gmail.com>
2021-09-10 12:58:42 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,5 @@
 use lingebra::Matrix;
 
-
 fn main() {
-    let vec_a = vec![0.0, 1.0, 2.0, 3.0];
-    let vec_b = vec![1.0, 0.0, 1.0, 0.0];
-    let vec_c = vec![5.0, 5.0, 5.0, 5.0];
-
-    let matrix = lingebra::Matrix::new(vec![vec_a, vec_b, vec_c]);
-
-    println!("{}", &matrix);
+    println!("Here is a matrix: {}", Matrix::identity(10));
 }
